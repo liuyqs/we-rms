71dff197b6c797398f7223d0348e7664fd836363
liuyq <liuyongqiang@xinhua.org>
2018-09-27 11:20:28 +0800

    新建项目

---
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -0,0 +1,23 @@
+package com.bootdo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+@EnableTransactionManagement
+@ServletComponentScan
+@MapperScan("com.bootdo.*.dao")
+@SpringBootApplication
+@EnableCaching
+public class BootdoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootdoApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    we-rms启动成功      ヾ(◍°∇°◍)ﾉﾞ");
+    }
+}
